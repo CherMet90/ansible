b96d17c7110bb9e0dd0749f9ce5937d9c30b5ad6
CherMet90 <dddfrdd@gmail.com>
2024-04-02 16:23:18 +0300

    project structure optimizing

--- netbox/netbox_inventory.py
+++ netbox/netbox_inventory.py
@@ -1,7 +1,8 @@
-import json
 import yaml
+
 from custom_modules.netbox_connector import NetboxDevice
 from custom_modules.log import logger
+
 
 # Load the configuration from the YAML file
 with open('netbox_inventory.yml', 'r') as file:
@@ -19,27 +20,17 @@ NetboxDevice.create_connection()
 devices = NetboxDevice.get_netbox_objects(path, action=action, **search_params)
 logger.info('Netbox generator successfully created')
 
-# Organize devices hierarchically
-hierarchy = {}
+# Organize devices dynamically into groups
+inventory = {'all': {'children': {}}}
 
 for device in devices:
     if device.device_type.manufacturer.slug == 'noname':
         continue
-    logger.info(f'Processing {device.name}...')
+    logger.info(f'Processing {device.display}...')
     site_name = device.site.slug if device.site else "Unknown"
     role_name = device.device_role.slug if device.device_role else "Unknown"
     manufacturer_name = device.device_type.manufacturer.slug if device.device_type.manufacturer else "Unknown"
     device_type_name = device.device_type.slug if device.device_type else "Unknown"
-
-    # Initialize dictionaries as needed
-    if site_name not in hierarchy:
-        hierarchy[site_name] = {}
-    if role_name not in hierarchy[site_name]:
-        hierarchy[site_name][role_name] = {}
-    if manufacturer_name not in hierarchy[site_name][role_name]:
-        hierarchy[site_name][role_name][manufacturer_name] = {}
-    if device_type_name not in hierarchy[site_name][role_name][manufacturer_name]:
-        hierarchy[site_name][role_name][manufacturer_name][device_type_name] = []
 
     # Fetch interfaces for the given device
     interfaces = NetboxDevice.get_netbox_objects('dcim.interfaces', action='filter', device_id=device.id)
@@ -50,18 +41,25 @@ for device in devices:
             "type": interface.type.value,
             "mode": interface.mode and str(interface.mode.value),
         })
-    
-    # Append the device to the appropriate list
-    hierarchy[site_name][role_name][manufacturer_name][device_type_name].append({
-        "name": device.name,
-        "primary_ip": device.primary_ip.address.split('/')[0] if device.primary_ip else "no_ip",
+
+    device_info = {
+        "ansible_host": device.primary_ip.address.split('/')[0] if device.primary_ip else "no_ip",
         "interfaces": device_interfaces,
-    })
-logger.info('Hierarchical organizing completed')
+    }
 
-# Формируем итоговый инвентарь
-inventory = {'hierarchy': hierarchy}
+    # Add device to groups
+    groups = [site_name, role_name, manufacturer_name, device_type_name]
+    for group in groups:
+        # Sanitize the group name
+        group = group.replace('-', '_')
+        if group not in inventory['all']['children']:
+            inventory['all']['children'][group] = {'hosts': {}}
+        inventory['all']['children'][group]['hosts'][device.display] = device_info
 
-logger.info('Creating JSON file...')
-with open('../inventories/prod/all_devices.json', 'w') as f:
-    json.dump(inventory, f, indent=4)
+logger.info('Dynamic hierarchical organizing completed')
+
+# Write the inventory into a YAML file
+inventory_path = '../inventories/prod/all_devices.yml'
+with open(inventory_path, 'w') as f:
+    yaml.dump(inventory, f, allow_unicode=True, sort_keys=False)
+logger.info('Ansible inventory YAML file created successfully')
